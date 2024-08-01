b205cef5289c187d3f4a3c0dba12f87f8c10f005
seoyulh92 <seoyulh92@gmail.com>
2024-08-01 17:18:07 +0900

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,12 +1,15 @@
-
 @echo off
 echo Creating virtual environment...
 python -m venv venv
 
 echo Activating virtual environment...
-call venv\Scripts\activate
+call venv\Scripts\activate.bat
+
+echo Ensuring pip is installed...
+python -m ensurepip --upgrade
 
 echo Installing required packages...
+pip install --upgrade pip
 pip install tkinter pynput SpeechRecognition python-Levenshtein
 
 echo Installation complete.
